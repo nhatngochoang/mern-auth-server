060604f2a43d4eac8cab6af8c468041f9e282e22
tahn-0102 <ongthoxunu@gmail.com>
2022-03-12 22:46:26 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+require("dotenv").config();
+
+const opts = {
+   useCreateIndex: true,
+   useFindAndModify: false,
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+}
+
+// db
+mongoose.connect(
+   process.env.MONGO_URL,
+   // opts,
+   (err) => {
+      if (err) throw err;
+      console.log("db connected")
+      // dont start server untill db connected
+      // routes
+      const PORT = 8000
+      app.listen(PORT, () => {
+         console.log('Server listening on port ' + PORT)
+      })
+   }
+);
+
+
+// mw
+
