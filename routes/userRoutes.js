d6719448feedf7249bab06c3fd2561f88fbb20c5
tahn-0102 <ongthoxunu@gmail.com>
2022-03-13 15:51:08 +0700

    # 20 - Forgot & Reset Password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const route = Router();
 const userController = require("../controllers/userController");
+const auth = require("../middlewares/auth");
 
 // register ➢ post data to server
 route.post("/api/auth/register", userController.register);
@@ -8,5 +9,8 @@ route.post("/api/auth/register", userController.register);
 route.post("/api/auth/activation", userController.activate);
 route.post("/api/auth/signing", userController.signing);
 route.post("/api/auth/access", userController.access);
+route.post("/api/auth/forgot_pass", userController.forgot);
+// actions like changing password, avatar ➢ user need to have server auth ➢ middleware check if access token is valid
+route.post("/api/auth/reset_pass", auth, userController.reset);
 
 module.exports = route;
